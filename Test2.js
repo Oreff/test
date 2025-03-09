78e377350c8901b00c10a04284397943fad0b571
oreff <emmasanyaolu1@gmail.com>
2025-03-09 04:55:06 -0300

    ore

---
+++ Test2.js
@@ -0,0 +1,12 @@
+const dataa = [1, 2, 3];
+
+let obj = JSON.stringify(dataa); //Convert A normal array or object to a JSON 
+console.log(obj);
+
+const json = `{"name" : "ore", 
+"employer" : {
+    "company" : "Mazda",
+    "numberOfJobs" : 20
+}}`;
+
+console.log(JSON.parse(json).employer.company) //Consvertss a JSON Type to Normal
